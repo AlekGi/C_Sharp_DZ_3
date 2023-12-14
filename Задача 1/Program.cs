08238a61513c855960cfb372fd0c223a222b0040
alek_rash <alekrash@gmail.com>
2023-12-14 21:20:09 +0300

    Initial commit

---
+++ Задача 1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 1: Задайте одномерный массив из 10 целых чисел от 1 до 100. 
+// Найдите количество элементов массива, значения которых лежат в отрезке [20,90].
+
+int[] array = new int[10];
+int count = 0;
+System.Console.WriteLine("Случайно сгенерированный массив:");
+for (int i = 0; i < array.Length; i++){
+    array[i] = new Random().Next(1, 100);
+    System.Console.Write(array[i] + " ");
+    
+    if (array[i] > 19 && array[i] < 91){
+        count++;
+    }
+} 
+
+System.Console.WriteLine("");
+System.Console.WriteLine("Количество элементов массива, которые лежат в отрезке от 20 до 90:");
+System.Console.WriteLine(count);
---
+++ Задача 3/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 3: Задайте массив из вещественных чисел с ненулевой дробной частью. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+
+double[] array = [1.1, 0.3, 3.2, 1.5, 5.8, 4.2, 2.2, 9.5, 7.6, 6.2];
+double min = array[0];
+double max = array[0];
+double result = 0;
+System.Console.WriteLine("Наш массив: [1.1, 0.3, 3.2, 1.5, 5.8, 4.2, 2.2, 9.5, 7.6, 6.2]");
+
+for (int i = 0; i < array.Length; i++){
+    if (max < array[i]){
+        max = array[i];
+    }
+    if (min > array[i]){
+        min = array[i];
+    }
+}
+result = max - min;
+
+System.Console.WriteLine("_________________");
+System.Console.WriteLine($"Максимальное значение массива: {max}");
+System.Console.WriteLine($"Минимальное значение массива:  {min}");
+System.Console.WriteLine($"Разница между максимальным и минимальным элементов массива: {result}");
+
+
+
+
+
